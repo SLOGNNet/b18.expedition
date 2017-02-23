7a5631d0058441538ddc3ad77df2c33f44c88aa2
andrew.zubko <andrewzubko@a.local>
2017-02-23 05:42:43 +0200

    try to use configurable service locator

--- expedition-impl/src/main/java/com/bridge18/expedition/ExpeditionModule.java
+++ expedition-impl/src/main/java/com/bridge18/expedition/ExpeditionModule.java
@@ -6,13 +6,29 @@ import com.bridge18.expedition.services.objects.LoadService;
 import com.bridge18.expedition.services.objects.impl.LoadServiceImpl;
 import com.bridge18.expedition.worker.WorkerBootstrap;
 import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.api.ConfigurationServiceLocator;
+import com.lightbend.lagom.javadsl.api.ServiceLocator;
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+import play.Configuration;
+import play.Environment;
 
 public class ExpeditionModule extends AbstractModule implements ServiceGuiceSupport {
+    private final Environment environment;
+    private final Configuration configuration;
+
+    public ExpeditionModule(Environment environment, Configuration configuration) {
+        this.environment = environment;
+        this.configuration = configuration;
+    }
+
     @Override
     protected void configure() {
         bind(LoadService.class).to(LoadServiceImpl.class);
 
         bindServices(serviceBinding(LagomLoadService.class, LagomLoadServiceImpl.class));
+
+        if (environment.isProd()) {
+            bind(ServiceLocator.class).to(ConfigurationServiceLocator.class);
+        }
     }
 }
