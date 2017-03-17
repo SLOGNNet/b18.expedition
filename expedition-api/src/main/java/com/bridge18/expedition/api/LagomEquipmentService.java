b1d7d9199531c50e0cc87cebc0d11cb7e11c2391
Viktor Zikratyi <victor.zikratyi@bridge18.com>
2017-03-17 20:02:59 +0200

    Configured CORS

--- expedition-api/src/main/java/com/bridge18/expedition/api/LagomEquipmentService.java
+++ expedition-api/src/main/java/com/bridge18/expedition/api/LagomEquipmentService.java
@@ -8,6 +8,7 @@ import com.bridge18.expedition.dto.v1.EquipmentSummary;
 import com.bridge18.expedition.dto.v1.PaginatedSequence;
 import com.lightbend.lagom.javadsl.api.Descriptor;
 import com.lightbend.lagom.javadsl.api.Service;
+import com.lightbend.lagom.javadsl.api.ServiceAcl;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import com.lightbend.lagom.javadsl.api.transport.Method;
 
@@ -30,6 +31,10 @@ public interface LagomEquipmentService extends Service {
                 restCall(Method.DELETE, "/v1/api/expedition/equipment/:id", this::deleteEquipment),
                 restCall(Method.GET, "/v1/api/expedition/equipment/:id", this::getEquipment),
                 restCall(Method.GET, "/v1/api/expedition/equipment?pageNo&pageSize", this::getEquipmentSummaries)
-        ).withAutoAcl(true);
+        ).withAutoAcl(true)
+                .withServiceAcls(
+                        ServiceAcl.methodAndPath(Method.OPTIONS, "\\*")
+                );
+
     }
 }
